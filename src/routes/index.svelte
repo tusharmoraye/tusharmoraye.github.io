1048ed84b3460ab61f253feece708e9fa11702ae
Tushar Moraye <tusharmoraye@gmail.com>
2021-09-13 16:12:21 +0530

    fix component filename

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
 	import '../styles/styles.scss';
 
-	import Home from '../components/Home.svelte';
+	import Home from '../components/home.svelte';
 	import About from '../components/about.svelte';
 	// import Skills from '../components/skills.svelte';
 	import Work from '../components/work.svelte';
